31bf45d2e731334b637cf86294c9d72815a6e795
Yann Donnelly <110302997@umail.ucc.ie>
2013-10-30 23:18:01 +0000

    Tikhonov PDF

--- out/4PAM/plotPDF.m
+++ out/4PAM/plotPDF.m
@@ -14,8 +14,18 @@ size(timing)
 d_ab = (a-b)/3000000000;
 y = [d_ab];
 pick = [1,5,10,15,20];
+
+figure(1)
 plot(x,y(pick,:))
 grid on
 xlim([1.86,2.0])
 ylim([-0.0002,0.0002])
 legend(num2str(timing(pick),"%.2f\n"))
+
+locs = 100*ones(1,30);
+for index=3:30
+    locs(index) = find(y(index,:)<0,1);
+endfor
+
+actlocs = x(locs);
+#result = [timing',locs']
